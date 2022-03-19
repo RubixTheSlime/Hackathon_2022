52a657f5cb1790359f30b1ba476a3cdeac2d5780
Sam Moore <a02324493@usu.edu>
2022-03-18 20:57:24 -0600

    Made blocks show up on screen

--- src/Block.py
+++ src/Block.py
@@ -3,9 +3,11 @@ from pygame import Vector2
 from pygame import Surface
 
 class Block:
-    def __init__(self):
+    SIZE = 120
+    
+    def __init__(self, left=0, top=0):
         self.sprite = pygame.image.load('src/res/Block.png')
-        self.rect = self.surface.get_rect()
+        self.rect = self.sprite.get_rect(left=left, top=top, width = 120, height = 120)
 
     def update(self):
         pass
