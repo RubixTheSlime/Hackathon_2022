83a557c7d06495e22e7eed735ec4ef6f6175bb24
Lyle Lowry <lyleblowry@gmail.com>
2022-03-19 00:27:02 -0600

    60fps, escape quits

--- src/player.py
+++ src/player.py
@@ -13,31 +13,28 @@ class Player:
         self.velocity = Vector2(0, 0)
         self.sprite = pygame.image.load('src/res/Erik.png')
         self.rect = self.sprite.get_rect(center=(100, 100))
+        # self.rect.height *= 0.95
 
-    def handle_movement(self, inputState: InputState):
+    def handle_movement(self, inputState: InputState, dt):
         if inputState.jump:
             if self.grounded_frames_remaining:
-                self.jump_timer += 2
+                self.velocity.y = -1000
 
         target_speed = 400 * (inputState.right - inputState.left)
         if abs(self.velocity.x - target_speed) < 10:
             self.velocity.x = target_speed
         else:
-            self.velocity.x += (target_speed - self.velocity.x) * 0.13
+            self.velocity.x += (target_speed - self.velocity.x) * dt * 6
 
         if inputState.boost:
             pass
 
     def update(self, dt, blocks):
-        if self.jump_timer:
-            self.velocity.y = -600
-            self.jump_timer -= 1
+        target_speed = 1500
+        if abs(self.velocity.y - target_speed) < 10:
+            self.velocity.y = target_speed
         else:
-            target_speed = 1500
-            if abs(self.velocity.y - target_speed) < 10:
-                self.velocity.y = target_speed
-            else:
-                self.velocity.y += (target_speed - self.velocity.y) * 0.05
+            self.velocity.y += (target_speed - self.velocity.y) * dt * 1.4
 
         self.rect = self.rect.move((self.velocity.x*dt, 0))
         # detect x collision with blocks
@@ -53,10 +50,9 @@ class Player:
         # detect y collision with blocks
         for block in blocks:
             if self.rect.colliderect(block.rect):
-                self.jump_timer = 0
                 if self.velocity.y >= 0:
                     self.rect.bottom = block.rect.top
-                    self.grounded_frames_remaining = 10
+                    self.grounded_frames_remaining = 3
                 else: # self.velocity.y is negative
                     self.rect.top = block.rect.bottom
                 self.velocity.y = 0
