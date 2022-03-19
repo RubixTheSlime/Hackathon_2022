b582643b3f2133c6f00cfa0f97d41a0472092baf
Lyle Lowry <lyleblowry@gmail.com>
2022-03-19 00:06:36 -0600

    changed gravity

--- src/player.py
+++ src/player.py
@@ -8,33 +8,36 @@ from inputstate import InputState
 
 class Player:
     def __init__(self):
-        self.grounded = False
+        self.grounded_frames_remaining = 0
+        self.jump_timer = 0
         self.velocity = Vector2(0, 0)
         self.sprite = pygame.image.load('src/res/Erik.png')
         self.rect = self.sprite.get_rect(center=(100, 100))
 
     def handle_movement(self, inputState: InputState):
         if inputState.jump:
-            if self.grounded:
-                self.velocity.y = -1000
+            if self.grounded_frames_remaining:
+                self.jump_timer += 2
 
-        if inputState.left:
-            if self.velocity.x > -400:
-                self.velocity.x -= 10
-        elif inputState.right:
-            if self.velocity.x < 400:
-                self.velocity.x += 10
+        target_speed = 400 * (inputState.right - inputState.left)
+        if abs(self.velocity.x - target_speed) < 10:
+            self.velocity.x = target_speed
         else:
-            if abs(self.velocity.x) < 10:
-                self.velocity.x = 0
-            else:
-                self.velocity.x *= 0.85
+            self.velocity.x += (target_speed - self.velocity.x) * 0.13
 
         if inputState.boost:
             pass
 
     def update(self, dt, blocks):
-        self.velocity.y += (1000 * dt)
+        if self.jump_timer:
+            self.velocity.y = -600
+            self.jump_timer -= 1
+        else:
+            target_speed = 1500
+            if abs(self.velocity.y - target_speed) < 10:
+                self.velocity.y = target_speed
+            else:
+                self.velocity.y += (target_speed - self.velocity.y) * 0.05
 
         self.rect = self.rect.move((self.velocity.x*dt, 0))
         # detect x collision with blocks
@@ -48,15 +51,17 @@ class Player:
 
         self.rect = self.rect.move((0, self.velocity.y*dt))
         # detect y collision with blocks
-        self.grounded = False
         for block in blocks:
             if self.rect.colliderect(block.rect):
                 if self.velocity.y >= 0:
                     self.rect.bottom = block.rect.top
-                    self.grounded = True
+                    self.grounded_frames_remaining = 10
                 else: # self.velocity.y is negative
                     self.rect.top = block.rect.bottom
                 self.velocity.y = 0
+
+        if self.grounded_frames_remaining > 0:
+            self.grounded_frames_remaining -= 1
 
     def draw(self, surface: Surface):
         surface.blit(self.sprite, self.rect)
