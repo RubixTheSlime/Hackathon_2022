c38a8e79b8723bd56f538c65e1aae5ad7532f357
Lyle Lowry <lyleblowry@gmail.com>
2022-03-19 14:01:14 -0600

    randomized texture orientations

--- src/backgroundblock.py
+++ src/backgroundblock.py
@@ -11,12 +11,15 @@ class BackgroundBlock:
     def __init__(self, top: int = None, left: int = None, copy_from: 'BackgroundBlock' = None):
         if copy_from is not None:
             self.path = copy_from.path
-            self.sprite = pygame.image.load(self.path).convert()
             self.topleft = copy_from.topleft[:]
+            self.flip_x, self.flip_y = copy_from.flip_x, copy_from.flip_y
+            self.sprite = pygame.transform.flip(pygame.image.load(self.path).convert(), self.flip_x, self.flip_y)
             return
         path_start = 'src/res/BackgroundBlock'
         self.path = os.path.join(path_start, random.choice(os.listdir(path_start)))
-        self.sprite = pygame.image.load(self.path).convert()
+        self.flip_x = random.randint(0, 1)
+        self.flip_y = random.randint(0, 1)
+        self.sprite = pygame.transform.flip(pygame.image.load(self.path).convert(), self.flip_x, self.flip_y)
         self.topleft = (top * 120, left * 120)
 
     def draw(self, surface: Surface) -> None:
--- src/block.py
+++ src/block.py
@@ -28,13 +28,20 @@ class Block:
         if copy_from is not None:
             self.block_type = copy_from.block_type
             self.path = copy_from.path
-            self.sprite = pygame.image.load(self.path).convert()
             self.rect = Rect(copy_from.rect)
+            self.flip_x, self.flip_y = copy_from.flip_x, copy_from.flip_y
+            self.sprite = pygame.transform.flip(pygame.image.load(self.path).convert(), self.flip_x, self.flip_y)
             return
+        self.flip_x = self.flip_y = False
+        if block_type in (BlockType.NORMAL, BlockType.FRAGILE, BlockType.SEMISOLID):
+            self.flip_x = random.randint(0, 1)
+        if block_type in (BlockType.NORMAL, BlockType.FRAGILE):
+            self.flip_y = random.randint(0, 1)
+
         self.block_type = block_type
         path_start = f'src/res/{Block.block_type_name[block_type]}'
         self.path = os.path.join(path_start, random.choice(os.listdir(path_start)))
-        self.sprite = pygame.image.load(self.path).convert()
+        self.sprite = pygame.transform.flip(pygame.image.load(self.path).convert(), self.flip_x, self.flip_y)
         self.rect = self.sprite.get_rect(left=left * Block.SIZE, top=top * Block.SIZE, width=Block.SIZE,
                                          height=Block.SIZE)
 
